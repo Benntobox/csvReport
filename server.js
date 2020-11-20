2cb829b19a80e0e0a1359eff52f6aa4cba88c684
Benntobox <benjaminsboyle3@gmail.com>
2020-11-20 12:19:22 -0800

    Fixed file structure

--- server.js
+++ server.js
@@ -1,4 +1,15 @@
-const app = require('./app.js');
+var express = require('express');
+var app = express();
+var path = require('path');
 const port = 3000;
+
+app.use(express.static('client'));
+
+app.post('/', (req, res) => {
+  console.log('Form was posted');
+  console.log(req.body)
+  console.log(res.body)
+  res.status(200).json(req.json);
+});
 
 app.listen(port, () => console.log('Listening!'));
