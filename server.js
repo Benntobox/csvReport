c81160675e9c64cea9e4b00d350c0ff82fb2ea36
Benntobox <benjaminsboyle3@gmail.com>
2020-11-20 13:03:53 -0800

    Successfully getting data from request

--- server.js
+++ server.js
@@ -1,15 +1,19 @@
 var express = require('express');
 var app = express();
-var path = require('path');
+var bodyparser = require('body-parser');
 const port = 3000;
 
+app.use(bodyparser.urlencoded({ extended: false }));
 app.use(express.static('client'));
+
+app.get('/', (req, res) => {
+  console.log('Getting', req.body);
+});
 
 app.post('/', (req, res) => {
   console.log('Form was posted');
-  console.log(req.body)
-  console.log(res.body)
-  res.status(200).json(req.json);
+  console.log(req.body['data']);
+  res.status(200).location('/');
 });
 
 app.listen(port, () => console.log('Listening!'));
